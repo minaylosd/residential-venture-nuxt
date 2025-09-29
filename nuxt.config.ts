b23f6050d0def6920f7f23be651794bece6ab983
minaylos-dev <minaylos.dev@gmail.com>
2025-09-29 11:44:31 +0300

    disable dinamic pages prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      routes: [
+        '/', '/blog', '/form', '/resident-program', '/venture-club', '/cases', '/articles', '/news'
+      ],
+    },
+  },
   css: ["~/assets/main.css"],
   postcss: {
     plugins: {
