1a43a7f5c1fedaaf8b53b82dd958ad575348caf0
minaylos-dev <minaylos.dev@gmail.com>
2025-09-29 12:25:17 +0300

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  nitro: {
-    prerender: {
-      routes: [
-        '/', '/blog', '/form', '/resident-program', '/venture-club'
-      ],
-    },
-  },
+  ssr: false,
   css: ["~/assets/main.css"],
   postcss: {
     plugins: {
